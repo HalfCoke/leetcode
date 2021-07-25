97573b5dbdade6cd480876620a7b58adb85320f7
HalfCoke <halfcokey@gmail.com>
2021-07-25 20:25:56 +0800

    进度30

---
+++ src/main/java/leetcode/problem25/Solution.java
@@ -0,0 +1,52 @@
+package leetcode.problem25;
+
+public class Solution {
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+
+        ListNode l1 = new ListNode(1);
+        l1.next = new ListNode(2);
+        l1.next.next = new ListNode(4);
+
+        ListNode l2 = new ListNode(1);
+        l2.next = new ListNode(3);
+        l2.next.next = new ListNode(4);
+
+        ListNode newList = solution.mergeTwoLists(l1, l2);
+        while (newList != null) {
+            System.out.println(newList.val);
+            newList = newList.next;
+        }
+    }
+
+    public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+        if (l1 == null) return l2;
+        if (l2 == null) return l1;
+
+        ListNode dummyHead = new ListNode(-1);
+        ListNode cur = dummyHead;
+
+        while (l1 != null && l2 != null) {
+            if (l1.val < l2.val) {
+                l1 = addNodeToNew(cur, l1);
+            } else {
+                l2 = addNodeToNew(cur, l2);
+            }
+            cur = cur.next;
+        }
+
+        if (l1 != null) {
+            cur.next = l1;
+        }
+        if (l2 != null) {
+            cur.next = l2;
+        }
+
+        return dummyHead.next;
+    }
+
+    private ListNode addNodeToNew(ListNode newList, ListNode node) {
+        newList.next = node;
+        return node.next;
+    }
+}
---
+++ src/main/java/leetcode/problem26/TreeNode.java
@@ -0,0 +1,11 @@
+package leetcode.problem26;
+
+public class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode(int x) {
+        val = x;
+    }
+}
