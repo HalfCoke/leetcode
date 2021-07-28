b4cc53d9a1cb79356e219086eaa5640b295ed635
HalfCoke <halfcokey@gmail.com>
2021-07-28 15:24:29 +0800

    进度36

---
+++ src/main/java/leetcode/problem35/Solution.java
@@ -0,0 +1,37 @@
+package leetcode.problem35;
+
+import java.util.HashMap;
+
+public class Solution {
+    public static void main(String[] args) {
+
+    }
+
+    public Node copyRandomList(Node head) {
+        if (head == null) return null;
+        HashMap<Node, Node> nodeMap = new HashMap<>();
+        Node cur = head;
+        Node pre = null;
+        while (cur != null) {
+            Node newNode = new Node(cur.val);
+            nodeMap.put(cur, newNode);
+
+            if (pre != null) {
+                Node node = nodeMap.get(pre);
+                node.next = newNode;
+            }
+            pre = cur;
+            cur = cur.next;
+        }
+
+        cur = head;
+        while (cur != null) {
+            if (cur.random != null) {
+                Node node = nodeMap.get(cur);
+                node.random = nodeMap.get(cur.random);
+            }
+            cur = cur.next;
+        }
+        return nodeMap.get(head);
+    }
+}
